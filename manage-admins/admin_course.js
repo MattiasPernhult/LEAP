0f6f03637455d635fd65a90fdb40d48e7ad2e254
Mattias Pernhult <pernhultmattias@gmail.com>
2016-03-02 13:03:33 +0100

    Added scripts for handling admins

---
+++ manage-admins/admin_course.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var Admin = require('./admin');
+
+mongoose.connect('mongodb://localhost/sandbox');
+
+var code = process.env.COURSECODE;
+var adminEmail = process.env.EMAIL;
+
+if (!code) {
+    console.error('Must provide a course code');
+    process.exit(1);
+}
+
+if (!adminEmail) {
+    console.error('Must provide an email');
+    process.exit(1);
+}
+
+Admin.findOne({email: adminEmail}, function(err, admin) {
+    if (!admin) {
+        console.error('Admin doesn\'t exists');
+        process.exit(1);
+    }
+    for (var i = 0; i < admin.courseCode.length; i++) {
+      if (admin.courseCode[i] === code) {
+        console.error('Course code already exists');
+        process.exit(1);
+      }
+    }
+    Admin.findOneAndUpdate(
+      {email: adminEmail},
+      {$push: {courseCode: code}},
+      {safe: true, upsert: true},
+      function(err, result) {
+        if (err) {
+          console.error('Problems with adding the course code');
+        } else {
+          console.log('Course code added for the specified admin');
+        }
+        process.exit(0);
+      });
+});
---
+++ manage-admins/app.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+var Admin = require('./admin');
+
+mongoose.connect('mongodb://localhost/sandbox');
+
+var adminEmail = process.env.EMAIL;
+
+if (!adminEmail) {
+    console.error('Must provide an email');
+    process.exit(1);
+}
+
+var newAdmin = new Admin();
+newAdmin.email = adminEmail;
+newAdmin.courseCode = [];
+
+Admin.findOne({email: adminEmail}, function(err, admin) {
+    if (admin) {
+        console.error('Admin already exists');
+        process.exit(1);
+    }
+    newAdmin.save(function(err) {
+        if (err) {
+            console.error('Problem with saving admin');
+            console.error(err);
+            process.exit(1);
+        }
+        console.log('Admin successfully added');
+        process.exit(0);
+    });
+});
